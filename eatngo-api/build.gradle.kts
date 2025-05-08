a2c13bb07804ef21cb4e9c258115302fe2e08692
hj4645 <hjpark4645@gmail.com>
2025-05-08 23:13:46 +0900

    feat: 불필요한 파일 제거
    - gradle wrapper가 필요없는 모듈의 파일들 제거

--- eatngo-api/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    kotlin("jvm") version "1.9.22"
-}
-
-dependencies {
-    implementation(project(":eatngo-api:eatngo-customer-api"))
-    implementation(project(":eatngo-api:eatngo-store-owner-api"))
-}
