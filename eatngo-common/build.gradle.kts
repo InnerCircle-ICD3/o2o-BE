b900662828e0cc1a2e80bd18c979a19b64e56f84
hj4645 <hjpark4645@gmail.com>
2025-05-08 23:17:30 +0900

    feat: Swagger 추가
    - common 모듈에 Swagger 설정 및 필요 의존성 추가
    - customer, store owner 모듈에 yml 추가

--- eatngo-common/build.gradle.kts
+++ eatngo-common/build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
 }
 
 dependencies {
+    // API 문서화
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")    // Swagger UI
+    implementation("org.springdoc:springdoc-openapi-starter-common:2.8.5")       // Swagger 공통 기능
 }
