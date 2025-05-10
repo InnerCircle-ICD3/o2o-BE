3791a877eeaf8b7aa1325f2e5d233ce54ddb1640
hj4645 <hjpark4645@gmail.com>
2025-05-10 16:04:35 +0900

    refactor: test controller DescribeSpec으로 변경 및 테스트

--- eatngo-customer-api/src/test/kotlin/com/eatngo/customer/HelloControllerTest.kt
+++ eatngo-customer-api/src/test/kotlin/com/eatngo/customer/HelloControllerTest.kt
@@ -1,15 +1,17 @@
 package com.eatngo.customer
 
-import com.eatngo.kotest.KotestBase
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
-import org.springframework.http.HttpStatus
 
-class HelloControllerTest : KotestBase() {
-    init {
-        "GET /api/v1/test/hello는 Hello, Swagger! 저는 사용자예요! 를 반환한다" {
-            val response = restTemplate.getForEntity("/api/v1/test/hello", String::class.java)
-            response.statusCode shouldBe HttpStatus.OK
-            response.body shouldBe "Hello, Swagger! 저는 사용자예요!"
+class HelloControllerTest : DescribeSpec({
+    val controller = HelloController()
+
+    describe("HelloController") {
+        context("/api/v1/test/hello 엔드포인트 호출 시") {
+            it("Hello, Swagger! 저는 사용자예요! 를 반환한다") {
+                val result = controller.hello()
+                result shouldBe "Hello, Swagger! 저는 사용자예요!"
+            }
         }
     }
-}
+})
