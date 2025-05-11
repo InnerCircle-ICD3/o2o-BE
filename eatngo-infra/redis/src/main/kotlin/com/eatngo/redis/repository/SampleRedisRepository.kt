7f60e21db20180aa212b27cbab78351fd5051065
youngDaLee <ldy0956@gmail.com>
2025-05-11 22:18:48 +0900

    chore: redis 모듈 설정

---
+++ eatngo-infra/redis/src/main/kotlin/com/eatngo/redis/repository/SampleRedisRepository.kt
@@ -0,0 +1,21 @@
+package com.eatngo.redis.repository
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Repository
+
+@Repository
+class SampleRedisRepository (
+    private val redisTemplate: RedisTemplate<String, String>
+) {
+    fun save(key: String, value: String) {
+        redisTemplate.opsForValue().set(key, value)
+    }
+
+    fun findByKey(key: String): String? {
+        return redisTemplate.opsForValue().get(key)
+    }
+
+    fun deleteByKey(key: String) {
+        redisTemplate.delete(key)
+    }
+}
