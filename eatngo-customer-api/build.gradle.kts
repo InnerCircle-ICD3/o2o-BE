be97fa63ac126900d2f4a9d803bdfc731edbd639
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-05-12 20:10:57 +0900

    feat: 테스트 의존성 추가

--- eatngo-customer-api/build.gradle.kts
+++ eatngo-customer-api/build.gradle.kts
@@ -25,4 +25,6 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
     testImplementation("io.kotest:kotest-assertions-core:5.8.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-property:5.8.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
