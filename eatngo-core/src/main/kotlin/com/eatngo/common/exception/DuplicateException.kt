d9e038365e365d2f93c91eaa9e825a3df2093e97
hj4645 <hjpark4645@gmail.com>
2025-05-13 23:53:27 +0900

    chore: exception 리팩토링 위해 불필요한 파일 제거

--- eatngo-core/src/main/kotlin/com/eatngo/common/exception/DuplicateException.kt
+++
@@ -1,9 +0,0 @@
-package com.eatngo.common.exception
-
-open class DuplicateException(
-    val value: String,
-    errorCode: ErrorCode = ErrorCode.DUPLICATE_ENTITY,
-    message: String = "$value 값이 이미 존재합니다.",
-    data: Map<String, Any>? = mapOf("duplicateValue" to value),
-    cause: Throwable? = null
-) : BusinessException(errorCode, message, data, cause) 
