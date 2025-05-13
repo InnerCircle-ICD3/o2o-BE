c9944c4581ed13a4bba56900d9b06b80d20592a4
hj4645 <hjpark4645@gmail.com>
2025-05-13 23:57:03 +0900

    chore: 테스트들 위한 hello controller와 hello controller test 수정(테스트 통과 완료)

--- eatngo-customer-api/src/main/kotlin/com/eatngo/customer/HelloController.kt
+++ eatngo-customer-api/src/main/kotlin/com/eatngo/customer/HelloController.kt
@@ -1,19 +1,34 @@
 package com.eatngo.customer
 
+import com.eatngo.common.error.BusinessErrorCode
+import com.eatngo.common.exception.BusinessException
+import com.eatngo.common.response.ApiResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.slf4j.LoggerFactory
 
 @Tag(name = "테스트", description = "Swagger 테스트용 API")
 @RestController
 @RequestMapping("/api/v1/test")
 class HelloController {
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     @Operation(summary = "Hello API", description = "사용자용 Swagger 테스트를 위한 Hello API")
     @GetMapping("/hello")
-    fun hello(): String {
-        return "Hello, Swagger! 저는 사용자예요!"
+    fun hello(): ApiResponse<String> {
+        return ApiResponse.success("Hello, Swagger! 저는 사용자예요!")
+    }
+
+    @Operation(summary = "Error API", description = "에러 테스트를 위한 API")
+    @GetMapping("/error")
+    fun error(): ApiResponse<String> {
+        throw BusinessException.of(
+            errorCode = BusinessErrorCode.ORDER_NOT_FOUND,
+            message = "테스트 주문 오류",
+            data = mapOf("orderId" to "123")
+        )
     }
 }
--- eatngo-customer-api/src/test/kotlin/com/eatngo/customer/HelloControllerTest.kt
+++ eatngo-customer-api/src/test/kotlin/com/eatngo/customer/HelloControllerTest.kt
@@ -1,16 +1,36 @@
 package com.eatngo.customer
 
+import com.eatngo.common.error.BusinessErrorCode
+import com.eatngo.common.exception.BusinessException
+import com.eatngo.common.response.ApiResponse
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import org.slf4j.LoggerFactory
 
 class HelloControllerTest : DescribeSpec({
-    val controller = HelloController()
+    val log = LoggerFactory.getLogger(this::class.java)
 
     describe("HelloController") {
-        context("/api/v1/test/hello 엔드포인트 호출 시") {
-            it("Hello, Swagger! 저는 사용자예요! 를 반환한다") {
+        context("정상 요청") {
+            it("공통 성공 응답 반환") {
+                val controller = HelloController()
                 val result = controller.hello()
-                result shouldBe "Hello, Swagger! 저는 사용자예요!"
+                result shouldBe ApiResponse.success("Hello, Swagger! 저는 사용자예요!")
+            }
+        }
+
+        context("에러 케이스") {
+            it("BusinessException 발생 및 에러 코드 검증") {
+                val controller = HelloController()
+                val exception = shouldThrow<BusinessException> {
+                    controller.error()
+                }
+
+                // 에러 코드 검증
+                exception.errorCode.code shouldBe BusinessErrorCode.ORDER_NOT_FOUND.code
+                exception.message shouldBe "테스트 주문 오류"
+                exception.data?.get("orderId") shouldBe "123"
             }
         }
     }
