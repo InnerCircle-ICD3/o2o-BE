4b0cdef5f57812d9ebc0ac916e32c2ec06ee83da
hj4645 <hjpark4645@gmail.com>
2025-05-13 23:55:14 +0900

    chore: 커스텀 exception인 BusinessException 리팩토링

--- eatngo-core/src/main/kotlin/com/eatngo/common/exception/BusinessException.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/common/exception/BusinessException.kt
@@ -1,18 +1,22 @@
 package com.eatngo.common.exception
 
+import com.eatngo.common.error.ErrorCode
+
 open class BusinessException(
     val errorCode: ErrorCode,
     override val message: String = errorCode.message,
     val data: Map<String, Any>? = null,
-    val cause: Throwable? = null
+    override val cause: Throwable? = null
 ) : RuntimeException(message, cause) {
-    // Stack 생성 비용 최소화를 위해 fillInStackTrace 오버라이딩
-    override fun fillInStackTrace(): Throwable {
-        return if (errorCode.logLevel.toInt() >= org.slf4j.event.Level.ERROR.toInt()) {
-            // ERROR 이상 로그 레벨에서만 스택 트레이스 수집
-            super.fillInStackTrace()
-        } else {
-            this
-        }
+
+    companion object {
+        fun of(errorCode: ErrorCode): BusinessException =
+            BusinessException(errorCode)
+
+        fun of(errorCode: ErrorCode, message: String): BusinessException =
+            BusinessException(errorCode, message)
+
+        fun of(errorCode: ErrorCode, message: String, data: Map<String, Any>): BusinessException =
+            BusinessException(errorCode, message, data = data)
     }
-} 
+}
