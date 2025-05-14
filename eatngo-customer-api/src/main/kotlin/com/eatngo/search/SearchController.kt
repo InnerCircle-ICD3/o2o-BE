55bec1d47c655ec409b589f3b66ff7771ee6eaa6
youngDaLee <ldy0956@gmail.com>
2025-05-15 00:56:48 +0900

    feat: 위치 기반 검색 스켈레톤 코드 구현

--- eatngo-customer-api/src/main/kotlin/com/eatngo/search/SearchController.kt
+++ eatngo-customer-api/src/main/kotlin/com/eatngo/search/SearchController.kt
@@ -4,23 +4,34 @@ import com.eatngo.search.dto.SearchStoreDto
 import com.eatngo.search.dto.SearchStoreMapRequestDto
 import com.eatngo.search.dto.SearchStoreMapResultDto
 import com.eatngo.search.dto.SearchStoreRequestDto
-import com.eatngo.search.dto.SearchStoreResultDto
+import com.eatngo.search.dto.SearchStoreResponseDto
 import com.eatngo.search.service.SearchService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * SearchController.kt
+ * @author : 이다영
+ * @date : 2025-05-14
+ * @description : 검색 관련 컨트롤러
+ */
 @RestController
 class SearchController (
     private val searchService: SearchService
 ) {
+    /**
+     * 리스트 검색 API
+     */
     @GetMapping("/search/store")
-    fun searchStore(@RequestParam searchDto: SearchStoreRequestDto): SearchStoreResultDto {
-        // Simulate a search operation
-        return searchService.searchStore(
-            SearchStoreDto(
-                viewPoint = searchDto.viewPoint,
-                filter = searchDto.filter
+    fun searchStore(@RequestParam searchDto: SearchStoreRequestDto): SearchStoreResponseDto {
+        return SearchStoreResponseDto(
+            success = true,
+            data = searchService.searchStore(
+                SearchStoreDto(
+                    viewPoint = searchDto.viewPoint,
+                    filter = searchDto.filter
+                )
             )
         )
     }
