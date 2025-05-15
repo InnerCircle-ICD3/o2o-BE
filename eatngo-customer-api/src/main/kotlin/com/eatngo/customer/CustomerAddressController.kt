6802d15fbaba32c73af6831df3d42646ff8d7f5b
youngDaLee <ldy0956@gmail.com>
2025-05-16 08:25:15 +0900

    feat: Swagger에서 보이도록 ResponseEntity로 수정 -> 추후 공통모듈로 수정 예정

--- eatngo-customer-api/src/main/kotlin/com/eatngo/customer/CustomerAddressController.kt
+++ eatngo-customer-api/src/main/kotlin/com/eatngo/customer/CustomerAddressController.kt
@@ -1,11 +1,11 @@
 package com.eatngo.customer
 
-import com.eatngo.common.response.ApiResponse
 import com.eatngo.customer.domain.CustomerAddress
 import com.eatngo.customer.dto.AddAddressRequestDto
 import com.eatngo.customer.service.CustomerAddressService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -21,14 +21,14 @@ class CustomerAddressController (
 
     @Operation(summary = "주소 목록 API", description = "유저의 주소 목록을 조회하는 API")
     @GetMapping("/api/v1/customer/address")
-    fun getAddressList(): ApiResponse<List<CustomerAddress>> {
-        return ApiResponse.success(customerAddressService.getAddressList())
+    fun getAddressList(): ResponseEntity<List<CustomerAddress>> {
+        return  ResponseEntity.ok(customerAddressService.getAddressList())
     }
 
     @Operation(summary = "주소 등록 API", description = "유저의 주소를 등록하는 API")
     @PostMapping("/api/v1/customer/address")
-    fun addAddress(@RequestBody address: AddAddressRequestDto): ApiResponse<Long/*AddressID*/> {
-        return ApiResponse.success(customerAddressService.addAddress(
+    fun addAddress(@RequestBody address: AddAddressRequestDto): ResponseEntity<Long/*AddressID*/> {
+        return ResponseEntity.ok(customerAddressService.addAddress(
             CustomerAddress(
                 id = 0L,
                 address = address.address,
@@ -40,7 +40,7 @@ class CustomerAddressController (
 
     @Operation(summary = "주소 삭제 API", description = "유저의 주소를 삭제하는 API")
     @DeleteMapping("/api/v1/customer/address/{addressId}")
-    fun deleteAddress(@PathVariable addressId: Long): ApiResponse<Boolean> {
-        return ApiResponse.success(customerAddressService.deleteAddress(addressId))
+    fun deleteAddress(@PathVariable addressId: Long): ResponseEntity<Boolean> {
+        return  ResponseEntity.ok(customerAddressService.deleteAddress(addressId))
     }
 }
--- eatngo-customer-api/src/main/kotlin/com/eatngo/search/SearchController.kt
+++ eatngo-customer-api/src/main/kotlin/com/eatngo/search/SearchController.kt
@@ -1,6 +1,5 @@
 package com.eatngo.search
 
-import com.eatngo.common.response.ApiResponse
 import com.eatngo.common.type.Point
 import com.eatngo.search.dto.SearchStoreMapResultDto
 import com.eatngo.search.dto.SearchStoreQueryDto
@@ -9,6 +8,7 @@ import com.eatngo.search.dto.SearchStoreResultDto
 import com.eatngo.search.service.SearchService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestParam
@@ -24,25 +24,26 @@ class SearchController (
     // TODO : 공통 모듈로 묶기
     @Operation(summary = "가게 검색 API", description = "매장 리스트 리턴 및 검색 API")
     @GetMapping("/api/v1/search/store")
-    fun searchStore(@ModelAttribute searchDto: SearchStoreRequestDto): ApiResponse<SearchStoreResultDto> {
-        return ApiResponse.success(
-            searchService.searchStore(
-                SearchStoreQueryDto(
-                    viewPoint = Point(
-                        lat = searchDto.lat,
-                        lng = searchDto.lng
-                    ),
-                    filter = searchDto.filter
+    fun searchStore(@ModelAttribute searchDto: SearchStoreRequestDto): ResponseEntity<SearchStoreResultDto> {
+        val searchResult = searchService.searchStore(
+            SearchStoreQueryDto(
+                viewPoint = Point(
+                    lat = searchDto.lat,
+                    lng = searchDto.lng
                 ),
-                offset = searchDto.offset
-            )
+                filter = searchDto.filter
+            ),
+            offset = searchDto.offset
+        )
+        return ResponseEntity.ok(
+            searchResult
         )
     }
 
     @Operation(summary = "지도 검색 API", description = "지도에서 매장 포인트 리턴 API")
     @GetMapping("/api/v1/search/store/map")
-    fun searchStoreMap(@RequestParam lat: Double, @RequestParam lng: Double): ApiResponse<SearchStoreMapResultDto> {
-        return ApiResponse.success(
+    fun searchStoreMap(@RequestParam lat: Double, @RequestParam lng: Double): ResponseEntity<SearchStoreMapResultDto> {
+        return ResponseEntity.ok(
             searchService.searchStoreMap(
                 SearchStoreQueryDto(
                     viewPoint = Point(
@@ -56,8 +57,8 @@ class SearchController (
 
     @Operation(summary = "검색어 자동완성 API", description = "검색어 자동완성 API ex: '치킨' -> '치킨, 치킨너겟'")
     @GetMapping("/api/v1/search/suggestions")
-    fun searchSuggestions(@RequestParam keyword: String): ApiResponse<List<String>> {
-        return ApiResponse.success(
+    fun searchSuggestions(@RequestParam keyword: String): ResponseEntity<List<String>> {
+        return ResponseEntity.ok(
             searchService.searchSuggestions(keyword)
         )
     }
