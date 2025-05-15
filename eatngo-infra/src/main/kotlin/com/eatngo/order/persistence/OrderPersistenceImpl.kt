3f68c4a0120480d9dd314d6b602ca809bbcf8611
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-05-15 18:31:56 +0900

    feat: 테스트 추가

--- eatngo-infra/src/main/kotlin/com/eatngo/order/persistence/OrderPersistenceImpl.kt
+++ eatngo-infra/src/main/kotlin/com/eatngo/order/persistence/OrderPersistenceImpl.kt
@@ -1,7 +1,6 @@
 package com.eatngo.order.persistence
 
 import com.eatngo.order.domain.Order
-import com.eatngo.order.domain.OrderItem
 import com.eatngo.order.infra.OrderPersistence
 import com.eatngo.order.rdb.entity.OrderJpaEntity
 import com.eatngo.order.rdb.repository.OrderRdbRepository
