c38cbe92f07a541743ae82c88667039a9d9072f4
youngDaLee <ldy0956@gmail.com>
2025-05-16 07:52:37 +0900

    feat: 매장 검색 결과

--- eatngo-core/src/main/kotlin/com/eatngo/search/dto/SearchStoreResultDto.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/search/dto/SearchStoreResultDto.kt
@@ -9,15 +9,17 @@ data class SearchStoreResultDto (
 data class SearchStore (
     val storeID: Long,
     val storeName: String,
+    val storeImage: String="", // 매장 이미지 URL
     val storeCategory: List<String>,
     val foodCategory: List<String>, // 대표 판매 음식 종류
     val distanceKm: Double,         // 검색하는 유저와 매장 간의 거리(km)
     val stock: Int,                 // 재고 수량
     val address: Address,           // 매장 주소
+
     // TODO: 리뷰, 찜 기능
-    val reviewCount: Int=0,         // 리뷰 수
-    val reviewScore: Double=5.0,    // 리뷰 평점
-    val isFavorite: Boolean=false,  // 찜 여부
+    val reviewCount: Int?=0,         // 리뷰 수
+    val reviewScore: Double?=5.0,    // 리뷰 평점
+    val isFavorite: Boolean?=false,  // 찜 여부
 )
 
 data class Address (
