b515868d5e44087702a48f8bdbe42a5d820e7cc7
youngDaLee <ldy0956@gmail.com>
2025-05-16 00:47:18 +0900

    feat: 주소 조회,등록,삭제 스켈레톤 코드 추가

--- eatngo-customer-api/src/main/kotlin/com/eatngo/customer/CustomerAddressController.kt
+++ eatngo-customer-api/src/main/kotlin/com/eatngo/customer/CustomerAddressController.kt
@@ -3,6 +3,7 @@ package com.eatngo.customer
 import com.eatngo.common.response.ApiResponse
 import com.eatngo.customer.domain.CustomerAddress
 import com.eatngo.customer.dto.AddAddressRequestDto
+import com.eatngo.customer.service.CustomerAddressService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -14,26 +15,32 @@ import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "Customer Address", description = "유저의 주소를 관리하는 API")
 @RestController
-class CustomerAddressController {
+class CustomerAddressController (
+    private val customerAddressService: CustomerAddressService
+) {
 
     @Operation(summary = "주소 목록 API", description = "유저의 주소 목록을 조회하는 API")
     @GetMapping("/customer/address")
     fun getAddressList(): ApiResponse<List<CustomerAddress>> {
-        // TODO : 주소 목록 API 구현
-        return ApiResponse.success(emptyList())
+        return ApiResponse.success(customerAddressService.getAddressList())
     }
 
     @Operation(summary = "주소 등록 API", description = "유저의 주소를 등록하는 API")
     @PostMapping("/customer/address")
     fun addAddress(@RequestBody address: AddAddressRequestDto): ApiResponse<Long/*AddressID*/> {
-        // TODO : 주소 등록 API 구현
-        return ApiResponse.success(1L)
+        return ApiResponse.success(customerAddressService.addAddress(
+            CustomerAddress(
+                id = 0L,
+                address = address.address,
+                point = address.point,
+                addressType = address.addressType
+            )
+        ))
     }
 
     @Operation(summary = "주소 삭제 API", description = "유저의 주소를 삭제하는 API")
     @DeleteMapping("/customer/address/{addressId}")
     fun deleteAddress(@PathVariable addressId: Long): ApiResponse<Boolean> {
-        // TODO : 주소 삭제 API 구현
-        return ApiResponse.success(true)
+        return ApiResponse.success(customerAddressService.deleteAddress(addressId))
     }
 }
