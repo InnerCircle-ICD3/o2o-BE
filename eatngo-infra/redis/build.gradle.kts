dddc231f14dc585728757a5da726dcd35e5da008
youngDaLee <ldy0956@gmail.com>
2025-05-17 22:14:34 +0900

    feat: redis 모듈 추가

---
+++ eatngo-infra/redis/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("org.springframework.boot")
+}
+
+dependencies {
+    // 내부 모듈 의존성
+    implementation(project(":eatngo-common:secrets"))
+
+    // Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+}
---
+++ eatngo-infra/redis/src/main/kotlin/com/eatngo/redis/RedisConfig.kt
@@ -0,0 +1,42 @@
+package com.eatngo.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableRedisRepositories
+class RedisConfig (
+    private val env: Environment
+) {
+
+    val stringSerializer: StringRedisSerializer = StringRedisSerializer()
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val host = env.getProperty("spring.redis.host")?: "localhost"
+        val port = env.getProperty("spring.redis.port")?.toInt() ?: 6379
+
+        val lettuceClientConfiguration = LettuceClientConfiguration.builder()
+            .build()
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration(host, port)
+
+        return LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration)
+    }
+
+    @Bean(value = ["redisTemplate"])
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<Any, Any> {
+        val template = RedisTemplate<Any, Any>()
+        template.connectionFactory = redisConnectionFactory
+        template.keySerializer  = stringSerializer
+        template.hashKeySerializer  = stringSerializer
+        return template
+    }
+}
