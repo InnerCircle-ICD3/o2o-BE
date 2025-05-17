1bbbaa1314e421c2942c6616d3b6cdc9a4b79273
youngDaLee <ldy0956@gmail.com>
2025-05-17 15:26:12 +0900

    feat: 검색필터 수정

--- eatngo-customer-api/src/main/kotlin/com/eatngo/search/dto/SearchStoreRequestDto.kt
+++ eatngo-customer-api/src/main/kotlin/com/eatngo/search/dto/SearchStoreRequestDto.kt
@@ -1,5 +1,7 @@
 package com.eatngo.search.dto
 
+import java.time.ZonedDateTime
+
 data class SearchStoreRequestDto (
     val lat: Double,            // 검색하는 유저의 위치 정보(위도)
     val lng: Double,            // 검색하는 유저의 위치 정보(경도)
@@ -8,7 +10,7 @@ data class SearchStoreRequestDto (
 )
 
 data class SearchFilter (
-    val category: String? = "", // 카테고리
-    val time:   Long? = null,     // 픽업 가능 시간(해당 시간 이후)
+    val category: String?,          // 카테고리
+    val time:   ZonedDateTime?,     // 픽업 가능 시간(해당 시간 이후)
     val status: StoreStatus = StoreStatus.ALL,    // 매장 상태
 )
