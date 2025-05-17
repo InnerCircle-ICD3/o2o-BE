56f9409dbb7f5501b9117aadf34dc8b127ad18d7
youngDaLee <ldy0956@gmail.com>
2025-05-17 15:18:59 +0900

    fix: 주소 도메인 모델 수정

--- eatngo-core/src/main/kotlin/com/eatngo/customer/domain/CustomerAddress.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/customer/domain/CustomerAddress.kt
@@ -2,9 +2,20 @@ package com.eatngo.customer.domain
 
 import com.eatngo.common.type.Point
 
-data class CustomerAddress (
+class CustomerAddress (
     val id: Long,        // 주소 ID
     val address: String, // 주소 한글명
     val point: Point,    // 위경도
     val addressType: AddressType, // 주소 타입 (도로명, 지번, 기타 등등)
-)
+) {
+    companion object {
+        fun create(): CustomerAddress {
+            return CustomerAddress(
+                id = 0L,
+                address = "",
+                point = Point(0.0, 0.0),
+                addressType = AddressType.ROAD
+            )
+        }
+    }
+}
--- eatngo-core/src/main/kotlin/com/eatngo/customer/service/CustomerAddressService.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/customer/service/CustomerAddressService.kt
@@ -10,15 +10,7 @@ class CustomerAddressService {
     fun getAddressList(): List<CustomerAddress> {
         // TODO : 주소 조회 구현
         return List(1) {
-            CustomerAddress(
-                id = 1L,
-                address = "서울특별시 강남구 테헤란로 123",
-                point = Point(
-                    lat = 37.123456,
-                    lng = 127.123456
-                ),
-                addressType = AddressType.ROAD
-            )
+            CustomerAddress.create()
         }
     }
 
