4683f43821e7fddf7321cd4f90bccee960f7f6a0
youngDaLee <ldy0956@gmail.com>
2025-05-17 11:57:39 +0900

    feat: ALB 헬스체크 컨트롤러 추가

---
+++ eatngo-store-owner-api/src/main/kotlin/com/eatngo/health/HealthCheckController.kt
@@ -0,0 +1,13 @@
+package com.eatngo.health
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+    @GetMapping("/health")
+    fun healthCheck(): ResponseEntity<String> {
+        return ResponseEntity.ok("OK")
+    }
+}
