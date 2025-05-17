2578d6151160c4b92bf65b7902955b11ba628871
youngDaLee <ldy0956@gmail.com>
2025-05-17 15:23:38 +0900

    feat: 검색 도메인 모델 추가

--- eatngo-core/src/main/kotlin/com/eatngo/search/domain/SearchStore.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/search/domain/SearchStore.kt
@@ -1,8 +1,36 @@
 package com.eatngo.search.domain
 
+import com.eatngo.common.type.Point
+import java.time.ZonedDateTime
+
 /**
  * SearchStore
- * 검색하는 가게의 정보를 담고 있는 클래스(MongoDB Document)
+ * 검색하는 가게의 정보를 담고 있는 클래스
+ * 삭제 시 RealDelete로 삭제 -> 원본 가게 데이터가 PostgreSQL에 남아있음
+ * TODO: MongoDB Document로 변환
  */
-class SearchStore {
+class SearchStore (
+    var storeId: Long = 0L,
+    var storeName: String = "",
+    var storeCategory: List<String> = emptyList(),
+    var foodCategory: List<String> = emptyList(),
+    var roadAddress: String = "",
+    var location: Point = Point(0.0, 0.0), // TODO: MongoDB GeoJSON으로 변환
+    var updatedAt: ZonedDateTime, // 마지막 업데이트 시간
+    var createdAt: ZonedDateTime, // 생성 시간
+) {
+    companion object {
+        fun create(): SearchStore {
+            return SearchStore(
+                storeId = 0L,
+                storeName = "",
+                storeCategory = emptyList(),
+                foodCategory = emptyList(),
+                roadAddress = "",
+                location = Point(0.0, 0.0),
+                updatedAt = ZonedDateTime.now(),
+                createdAt = ZonedDateTime.now()
+            )
+        }
+    }
 }
--- eatngo-core/src/main/kotlin/com/eatngo/search/dto/SearchStoreResultDto.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/search/dto/SearchStoreResultDto.kt
@@ -13,16 +13,13 @@ data class SearchStore (
     val storeCategory: List<String>,
     val foodCategory: List<String>, // 대표 판매 음식 종류
     val distanceKm: Double,         // 검색하는 유저와 매장 간의 거리(km)
+    val isOpen: Boolean,            // 매장 오픈 여부
     val stock: Int,                 // 재고 수량
-    val address: Address,           // 매장 주소
+    val roadAddress: String,        // 매장 주소(도로명 주소)
+    val location: Point,            // 매장 위치(위도, 경도)
 
     // TODO: 리뷰, 찜 기능
     val reviewCount: Int?=0,         // 리뷰 수
     val reviewScore: Double?=5.0,    // 리뷰 평점
     val isFavorite: Boolean?=false,  // 찜 여부
-)
-
-data class Address (
-    val address: String, // 도로명주소
-    val point: Point,    // 위경도
 )
