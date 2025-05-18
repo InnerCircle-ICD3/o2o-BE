604fa929728579b8953b62d5be3de540e10a0a96
youngDaLee <ldy0956@gmail.com>
2025-05-18 14:55:04 +0900

    feat: 완료된 Todo 삭제

--- eatngo-core/src/main/kotlin/com/eatngo/search/domain/SearchStore.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/search/domain/SearchStore.kt
@@ -8,7 +8,6 @@ import java.time.ZonedDateTime
  * SearchStore
  * 검색하는 가게의 정보를 담고 있는 클래스
  * 삭제 시 RealDelete로 삭제 -> 원본 가게 데이터가 PostgreSQL에 남아있음
- * TODO: MongoDB Document로 변환
  */
 class SearchStore (
     var storeId: Long = 0L,
@@ -17,7 +16,7 @@ class SearchStore (
     var storeCategory: List<String> = emptyList(),
     var foodCategory: List<String> = emptyList(),
     var roadAddress: String = "",
-    var location: Point = Point(0.0, 0.0), // TODO: MongoDB GeoJSON으로 변환
+    var location: Point = Point(0.0, 0.0),
     var open: Boolean = false,  // 매장 오픈 여부
     var openTime: LocalDateTime, // 매장 오픈 시간
     var closeTime: LocalDateTime, // 매장 마감 시간
