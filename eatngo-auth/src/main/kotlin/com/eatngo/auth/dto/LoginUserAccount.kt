69ea230765ec90c186b3e172514592c2750fd470
Kyungtae Park <pktpkt8917@gmail.com>
2025-05-27 20:02:58 +0900

    feat: 변경 누락분 반영

--- eatngo-auth/src/main/kotlin/com/eatngo/auth/dto/LoginUserAccount.kt
+++ eatngo-auth/src/main/kotlin/com/eatngo/auth/dto/LoginUserAccount.kt
@@ -2,5 +2,6 @@ package com.eatngo.auth.dto
 
 class LoginUserAccount(
     override val userAccountId: Long,
+    override val nickname: String? = null,
     override val roles: List<String>
 ) : LoginUser
