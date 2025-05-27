71b15b412511fd422b306c7d11bf7629ba8df091
Kyungtae Park <pktpkt8917@gmail.com>
2025-05-27 21:44:00 +0900

    feat: 잘못 작성한 메시지 수정

--- eatngo-core/src/main/kotlin/com/eatngo/user_account/vo/Nickname.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/user_account/vo/Nickname.kt
@@ -3,7 +3,7 @@ package com.eatngo.user_account.vo
 @JvmInline
 value class Nickname(val value: String) {
     init {
-        require(value.isNotBlank()) { "Email address cannot be blank" }
+        require(value.isNotBlank()) { "Nickname cannot be blank" }
         require(value.length <= 50) { "Nickname cannot exceed 50 characters" }
     }
 
