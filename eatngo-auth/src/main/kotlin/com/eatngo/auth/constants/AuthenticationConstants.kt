25be2774c140a62b42e23474ca1c84271b41d0d5
Kyungtae Park <pktpkt8917@gmail.com>
2025-05-27 19:49:11 +0900

    feat: 로그인 이후 닉네임이 없으면 리다이렉트 시키기

--- eatngo-auth/src/main/kotlin/com/eatngo/auth/constants/AuthenticationConstants.kt
+++ eatngo-auth/src/main/kotlin/com/eatngo/auth/constants/AuthenticationConstants.kt
@@ -3,6 +3,7 @@ package com.eatngo.auth.constants
 object AuthenticationConstants {
 
     const val PRINCIPAL_KEY = "userAccountId"
+    const val NICKNAME = "nickname"
 
     const val ACCESS_TOKEN = "access_token"
 }
