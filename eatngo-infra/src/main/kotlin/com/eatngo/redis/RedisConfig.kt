37aeb13f513437a944ac723dc5e6ec0273949279
suzhanlee <wlscww@kakao.com>
2025-05-30 10:34:13 +0900

    settings: Redis AOP 기반 설정 추가

--- eatngo-infra/src/main/kotlin/com/eatngo/redis/RedisConfig.kt
+++ eatngo-infra/src/main/kotlin/com/eatngo/redis/RedisConfig.kt
@@ -3,12 +3,16 @@ package com.eatngo.redis
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
 
@@ -44,5 +48,23 @@ class RedisConfig(
         template.keySerializer = stringSerializer
         template.hashKeySerializer = stringSerializer
         return template
+    }
+
+    @Bean
+    fun cacheManager(factory: RedisConnectionFactory): CacheManager {
+        val config = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            )
+            .entryTtl(Duration.ofMinutes(60))
+
+        return RedisCacheManager.builder(factory)
+            .cacheDefaults(config)
+            .build()
     }
 }
