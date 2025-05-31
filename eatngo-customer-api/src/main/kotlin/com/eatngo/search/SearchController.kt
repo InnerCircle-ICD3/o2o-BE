4aada2fce154f0b74f95adb5c01df24e9a528922
youngDaLee <ldy0956@gmail.com>
2025-05-31 10:46:21 +0900

    docs: swagger 파라미터 문서 추가

--- eatngo-customer-api/src/main/kotlin/com/eatngo/search/SearchController.kt
+++ eatngo-customer-api/src/main/kotlin/com/eatngo/search/SearchController.kt
@@ -34,10 +34,14 @@ class SearchController(
         @RequestParam longitude: Double,
         @Parameter(description = "필터링할 매장 카테고리", example = "BREAD")
         @RequestParam storeCategory: StoreEnum.StoreCategory?,
-        @RequestParam time: String?, // HH:mm 형식의 시간 (ex: 12:30) TODO: VO로 정의하여 검증 로직 추가
-        @RequestParam onlyReservable: Boolean = false, // 픽업 가능 매장만 조회
+        @Parameter(description = "HH:mm 형식의 시간 ", example = "12:30")
+        @RequestParam time: String?, // TODO: VO로 정의하여 검증 로직 추가
+        @Parameter(description = "픽업 가능 매장만 조회 여부 (null, false시 모든 상태 조회)", example = "true")
+        @RequestParam onlyReservable: Boolean = false,
         // TODO : 우선 BE, FE 모두 page+size로 구현 => 추후 개선
+        @Parameter(description = "페이지 번호", example = "0")
         @RequestParam page: Int = 0,
+        @Parameter(description = "페이지 사이즈", example = "20")
         @RequestParam size: Int = 20,
     ): ResponseEntity<SearchStoreResultDto> =
         ResponseEntity.ok(
@@ -57,11 +61,16 @@ class SearchController(
     @Operation(summary = "가게 검색 API", description = "매장 텍스트 검색 API")
     @GetMapping("/api/v1/search/store")
     fun searchStoreKeyword(
+        @Parameter(description = "중심 좌표의 위도 (latitude)", example = "37.566500")
         @RequestParam latitude: Double,
+        @Parameter(description = "중심 좌표의 경도 (latitude)", example = "126.978011")
         @RequestParam longitude: Double,
+        @Parameter(description = "검색어", example = "치킨")
         @RequestParam searchText: String,
         // TODO : 우선 BE, FE 모두 page+size로 구현 => 추후 개선
+        @Parameter(description = "페이지 번호", example = "0")
         @RequestParam page: Int = 0,
+        @Parameter(description = "페이지 사이즈", example = "20")
         @RequestParam size: Int = 20,
     ): ResponseEntity<SearchStoreResultDto> =
         ResponseEntity.ok(
@@ -79,7 +88,9 @@ class SearchController(
     @Operation(summary = "지도 검색 API", description = "지도에서 매장 포인트 리턴 API")
     @GetMapping("/api/v1/search/store/map")
     fun searchStoreMap(
+        @Parameter(description = "중심 좌표의 위도 (latitude)", example = "37.566500")
         @RequestParam latitude: Double,
+        @Parameter(description = "중심 좌표의 경도 (latitude)", example = "126.978011")
         @RequestParam longitude: Double,
     ): ResponseEntity<SearchStoreMapResultDto> =
         ResponseEntity.ok(
@@ -99,6 +110,7 @@ class SearchController(
     @Operation(summary = "검색어 자동완성 API", description = "검색어 자동완성 API ex: '치킨' -> '치킨, 치킨너겟'")
     @GetMapping("/api/v1/search/suggestions")
     fun searchSuggestions(
+        @Parameter(description = "검색어", example = "치킨")
         @RequestParam keyword: String,
     ): ResponseEntity<SearchSuggestionResultDto> =
         ResponseEntity.ok(
