0d2406546ed8f70f5c00cc6c2487721edca0e338
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-06-01 03:02:08 +0900

    feat: 구현하지 않은 persistence 메소드 구현

--- eatngo-core/src/main/kotlin/com/eatngo/product/service/ProductService.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/product/service/ProductService.kt
@@ -28,7 +28,6 @@ class ProductService(
     private val fileStorageService: FileStorageService,
     private val inventoryService: InventoryService,
     private val storePersistence: StorePersistence,
-    private val storeTotalInventoryTypeDecider: StoreTotalInventoryTypeDecider
 ) {
     @Transactional
     fun createProduct(
@@ -139,7 +138,11 @@ class ProductService(
         val savedProduct = productPersistence.save(product)
 
         val store: Store = storePersistence.findById(product.storeId).orThrow { StoreNotFound(product.storeId) }
-        val changedInventory: InventoryDto = inventoryService.toggleInventory(productCurrentStockDto, store.id)
+        val changedInventory: InventoryDto = inventoryService.toggleInventory(
+            productCurrentStockDto,
+            store.id,
+            findTotalInitialStocks(store.id)
+        )
 
         return ProductAfterStockDto.create(savedProduct, changedInventory)
     }
