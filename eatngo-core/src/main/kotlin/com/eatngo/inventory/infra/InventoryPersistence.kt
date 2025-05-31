672273c6d11ea530e4031fb57ef76693df0c0040
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-05-31 12:18:37 +0900

    refactor: lua script 수정 및 클래스 명 변경

--- eatngo-core/src/main/kotlin/com/eatngo/inventory/infra/InventoryPersistence.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/inventory/infra/InventoryPersistence.kt
@@ -6,5 +6,7 @@ interface InventoryPersistence {
     fun save(inventory: Inventory): Inventory
     fun findTopByProductIdOrderByVersionDesc(productId: Long): Inventory?
     fun deleteByProductId(productId: Long)
+
+    // 현재 점주만 사용 중 -> 점주가 재고를 토글 형식으로 증가/차감 시킬 때 사용
     fun updateStock(productId: Long, stockQuantity: Int): Int
 }
