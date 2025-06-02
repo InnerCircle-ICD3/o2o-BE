45adfcd790bf9740f2dd0f794b129689482294f1
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2025-06-02 20:33:03 +0900

    [FEAT] 리프레시 토큰 로직 추가 (#83)
    * feature/refresh-token 리프레시 토큰 로직 추가
    
    * feature/refresh-token 코드리뷰 반영

--- eatngo-auth/src/main/kotlin/com/eatngo/auth/dto/LoginStoreOwner.kt
+++ eatngo-auth/src/main/kotlin/com/eatngo/auth/dto/LoginStoreOwner.kt
@@ -1,8 +1,12 @@
 package com.eatngo.auth.dto
 
+import com.eatngo.user_account.oauth2.constants.Role
+
 data class LoginStoreOwner(
     override val userAccountId: Long,
     override val roles: List<String>,
     override val nickname: String?,
     val storeOwnerId: Long
-) : LoginUser
+) : LoginUser {
+    override fun getCurrentRole(): Role = Role.STORE_OWNER
+}
