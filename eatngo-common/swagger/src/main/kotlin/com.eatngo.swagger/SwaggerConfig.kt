6fa5d634dd1ee158f876e6e86cb857bfe58cfb94
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2025-06-03 22:02:35 +0900

    doc/swagger-authorize-ui 요청 인증 정보 세팅하기 (#105)

--- eatngo-common/swagger/src/main/kotlin/com.eatngo.swagger/SwaggerConfig.kt
+++ eatngo-common/swagger/src/main/kotlin/com.eatngo.swagger/SwaggerConfig.kt
@@ -1,5 +1,8 @@
 package com.eatngo.swagger
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import org.springframework.beans.factory.annotation.Value
@@ -7,6 +10,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@SecurityScheme(
+    name = "securityCookie",
+    type = SecuritySchemeType.APIKEY,
+    `in` = SecuritySchemeIn.COOKIE,
+    paramName = "access_token"
+)
 class SwaggerConfig(
     @Value("\${swagger.title}") private val title: String,
     @Value("\${swagger.description}") private val description: String,
