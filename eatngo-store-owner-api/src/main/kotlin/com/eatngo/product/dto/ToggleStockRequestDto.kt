5d506427ef31dd673a8972095d6101305434a41d
suzhanlee <wlscww@kakao.com>
2025-06-04 09:25:09 +0900

    feat: product swagger 문서 세부 항목 작성

--- eatngo-store-owner-api/src/main/kotlin/com/eatngo/product/dto/ToggleStockRequestDto.kt
+++ eatngo-store-owner-api/src/main/kotlin/com/eatngo/product/dto/ToggleStockRequestDto.kt
@@ -1,5 +1,6 @@
 package com.eatngo.product.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
@@ -9,7 +10,15 @@ data class ToggleStockRequestDto(
     val id: Long,
 
     @field:NotBlank(message = "재고의 증감을 선택해주세요.")
+    @field:Schema(
+        description = "재고 증가 or 감소 선택",
+        examples = ["increase", "decrease"]
+    )
     val action: String,
 
+    @field:Schema(
+        description = "재고 증/감 수량",
+        defaultValue = "1"
+    )
     val amount: Int = 1
 )
--- eatngo-store-owner-api/src/main/kotlin/com/eatngo/product/dto/UpdateProductRequestDto.kt
+++ eatngo-store-owner-api/src/main/kotlin/com/eatngo/product/dto/UpdateProductRequestDto.kt
@@ -1,5 +1,6 @@
 package com.eatngo.product.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.Valid
 import jakarta.validation.constraints.DecimalMax
 import jakarta.validation.constraints.DecimalMin
@@ -12,15 +13,38 @@ data class UpdateProductRequestDto(
     @field:Valid
     val price: UpdateProductPriceRequestDto,
 
+    @field:Schema(
+        description = "상품 사이즈",
+        examples = ["L(large)", "M(medium)", "S(small)"],
+    )
     val size: String,
+
     val inventory: UpdateProductInventoryRequestDto,
+
+    @field:Schema(
+        description = "잇고백 상품 세부 정보들",
+        examples = ["단팥빵", "소금빵", "팥빵"],
+    )
     val foodType: List<String>,
+
     val image: String?,
+
+    @field:Schema(
+        description = "상품 상태",
+        examples = ["ACTIVE(판매중)", "INACTIVE(판매x)", "SOLD_OUT(재고 소진으로 인한 판매 종료)"]
+    )
     val status: String?,
 )
 
 data class UpdateProductInventoryRequestDto(
+    @field:Schema(
+        description = "점수가 설정한 초기 수량"
+    )
     val quantity: Int,
+
+    @field:Schema(
+        description = "남아있는 상품의 현재 수량"
+    )
     val stock: Int,
 )
 
@@ -30,8 +54,10 @@ data class UpdateProductPriceRequestDto(
 
     @field:DecimalMin(value = "0.0", message = "할인율은 0.0 이상이어야 합니다")
     @field:DecimalMax(value = "1.0", message = "할인율은 1.0 이하여야 합니다")
+    @field:Schema(
+        description = "상품의 할인률",
+        example = "0.3",
+        defaultValue = "0.5"
+    )
     val discountRate: Double,
-
-    @field:Min(value = 0, message = "최종 가격은 0 이상이어야 합니다")
-    val finalPrice: Int
 )
