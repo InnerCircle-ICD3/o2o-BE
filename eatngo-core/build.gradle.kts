8415a8cadb8bb3f35380d366ccf2dad9495fb407
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-06-04 21:53:37 +0900

    gradle: sse emitter 추가

--- eatngo-core/build.gradle.kts
+++ eatngo-core/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 
 dependencies {
     // Spring Boot 기본 의존성
+
     implementation("org.springframework:spring-context")  // REST API 지원
+    implementation("org.springframework.boot:spring-boot-starter-web") // SSE Emitter 지원
     implementation("org.jetbrains.kotlin:kotlin-reflect")              // Kotlin 리플렉션 지원
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Kotlin JSON 직렬화/역직렬화
     implementation("org.springframework.boot:spring-boot-starter-data-jpa") // Transactional 사용
