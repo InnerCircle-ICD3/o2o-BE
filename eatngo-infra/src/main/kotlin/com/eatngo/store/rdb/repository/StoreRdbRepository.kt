bf8dca9cc54753011ae01aeffad5b5f6422fd596
youngDaLee <ldy0956@gmail.com>
2025-06-06 15:41:13 +0900

    feat: updatedAt 기준 store 가져오기

--- eatngo-infra/src/main/kotlin/com/eatngo/store/rdb/repository/StoreRdbRepository.kt
+++ eatngo-infra/src/main/kotlin/com/eatngo/store/rdb/repository/StoreRdbRepository.kt
@@ -1,11 +1,11 @@
 package com.eatngo.store.rdb.repository
 
 import com.eatngo.store.rdb.entity.StoreJpaEntity
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
-import java.util.*
+import java.time.LocalDateTime
+import java.util.Optional
 
 interface StoreRdbRepository : JpaRepository<StoreJpaEntity, Long> {
     @Query(
@@ -13,7 +13,8 @@ interface StoreRdbRepository : JpaRepository<StoreJpaEntity, Long> {
     select s from StoreJpaEntity s
     join fetch s.address
     where s.id = :id
-    """)
+    """,
+    )
     override fun findById(id: Long): Optional<StoreJpaEntity>
 
     @Query(
@@ -21,15 +22,27 @@ interface StoreRdbRepository : JpaRepository<StoreJpaEntity, Long> {
     select s from StoreJpaEntity s
     join fetch s.address
     where s.storeOwnerId = :storeOwnerId
-    """)
+    """,
+    )
     fun findByStoreOwnerIdWithAddress(storeOwnerId: Long): List<StoreJpaEntity>
 
-    @Query("""
+    @Query(
+        """
     select s from StoreJpaEntity s
     join fetch s.address
     where s.id in :ids
-    """)
+    """,
+    )
     fun findAllByIdIn(ids: List<Long>): List<StoreJpaEntity>
+
+    @Query(
+        """
+    select s from StoreJpaEntity s
+    join fetch s.address
+    where s.updatedAt > :pivotTime
+    """,
+    )
+    fun findByUpdatedAt(pivotTime: LocalDateTime): List<StoreJpaEntity>
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query(
@@ -37,7 +50,7 @@ interface StoreRdbRepository : JpaRepository<StoreJpaEntity, Long> {
         UPDATE StoreJpaEntity s
         SET s.deletedAt = CURRENT_TIMESTAMP
         WHERE s.id = :id
-    """
+    """,
     )
     fun softDeleteById(id: Long): Int
 }
