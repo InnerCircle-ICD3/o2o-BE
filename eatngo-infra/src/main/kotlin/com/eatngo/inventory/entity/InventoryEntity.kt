103b2f4fb00952d6f0ce95ad9a77d758c84d00ae
suzhanlee <wlscwww@gmail.com>
2025-06-07 15:44:47 +0900

    feat: Inventory 에 재고 날짜 추가

--- eatngo-infra/src/main/kotlin/com/eatngo/inventory/entity/InventoryEntity.kt
+++ eatngo-infra/src/main/kotlin/com/eatngo/inventory/entity/InventoryEntity.kt
@@ -2,6 +2,7 @@ package com.eatngo.inventory.entity
 
 import jakarta.persistence.*
 import org.springframework.data.redis.core.RedisHash
+import java.time.LocalDate
 
 @Entity
 @Table(name = "inventory")
@@ -12,6 +13,7 @@ data class InventoryEntity(
     var id: Long = 0L,
     val quantity: Int,
     val stock: Int,
+    val inventoryDate: LocalDate,
 
     @Column(name = "product_id", nullable = false)
     val productId: Long,
