eaff19cd02f5df807dce4c2c1d76f774fac369ed
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-06-09 20:42:34 +0900

    fix: StoreOwnerId 대신 StoreId를 사용하도록 변경 1:N 관계를 이용해 소유 관계인 Store 만 조회할 수 있도록 수정

--- eatngo-core/src/main/kotlin/com/eatngo/order/usecase/StoreOrderStatusChangedUseCase.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/order/usecase/StoreOrderStatusChangedUseCase.kt
@@ -1,10 +1,10 @@
 package com.eatngo.order.usecase
 
 import com.eatngo.order.domain.Status
-import com.eatngo.order.dto.OrderStatusChangedDto
+import com.eatngo.order.dto.StoreOrderStatusChangedDto
 import com.eatngo.order.event.OrderEvent
 import com.eatngo.order.service.OrderService
-import com.eatngo.store_owner.service.StoreOwnerService
+import com.eatngo.store.service.impl.StoreServiceImpl
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,12 +12,15 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class StoreOrderStatusChangedUseCase(
     private val orderService: OrderService,
-    private val storeService: StoreOwnerService,
+    private val storeService: StoreServiceImpl,
     private val eventPublisher: ApplicationEventPublisher
 ) {
     @Transactional
-    fun change(dto: OrderStatusChangedDto) {
-        val store = storeService.getStoreOwnerById(dto.userId)
+    fun change(dto: StoreOrderStatusChangedDto) {
+        val store = storeService.getStoreById(dto.userId)
+
+        require(store.storeOwnerId == dto.userId) { "자신의 상점만 조회할 수 있습니다." }
+
         val order = orderService.getById(dto.orderId)
 
         when (dto.status) {
--- eatngo-core/src/main/kotlin/com/eatngo/order/usecase/StoreReadOrderUseCase.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/order/usecase/StoreReadOrderUseCase.kt
@@ -2,22 +2,29 @@ package com.eatngo.order.usecase
 
 import com.eatngo.common.response.Cursor
 import com.eatngo.order.dto.OrderDto
-import com.eatngo.order.dto.OrderQueryParamDto
+import com.eatngo.order.dto.StoreOrderQueryParamDto
 import com.eatngo.order.service.OrderService
-import com.eatngo.store_owner.service.StoreOwnerService
+import com.eatngo.store.service.impl.StoreServiceImpl
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class StoreReadOrderUseCase(
     private val orderService: OrderService,
-    private val storeService: StoreOwnerService
+    private val storeService: StoreServiceImpl
 ) {
     @Transactional(readOnly = true)
-    fun findAllByQueryParameter(queryParam: OrderQueryParamDto): Cursor<OrderDto> {
-        storeService.getStoreOwnerById(queryParam.storeId!!)
+    fun findAllByQueryParameter(queryParam: StoreOrderQueryParamDto): Cursor<OrderDto> {
+        val store = storeService.getStoreById(queryParam.storeId)
+
+        require(store.storeOwnerId == queryParam.storeOwnerId) { "자신의 상점만 조회할 수 있습니다." }
+
         val cursoredOrders = orderService.findAllByQueryParam(queryParam)
 
-        return Cursor.from(cursoredOrders.contents.map { OrderDto.from(it) }, lastId = cursoredOrders.lastId)
+        return Cursor.from(
+            content = cursoredOrders.contents
+                .map { OrderDto.from(it) },
+            lastId = cursoredOrders.lastId
+        )
     }
 }
