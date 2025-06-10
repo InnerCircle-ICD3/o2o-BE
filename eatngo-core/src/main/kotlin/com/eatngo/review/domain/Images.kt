80a9d482a75eed21b79b3f3b9c10a7c382719979
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-06-10 15:34:38 +0900

    fix: review image 개수로 인한 문제 해결

--- eatngo-core/src/main/kotlin/com/eatngo/review/domain/Images.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/review/domain/Images.kt
@@ -1,13 +1,15 @@
 package com.eatngo.review.domain
 
 data class Images(
-    val images: List<String>
+    val images: List<String>,
 ) {
     init {
-        require(images.size > MAX_IMAGE_COUNT) { "이미지의 개수는 $MAX_IMAGE_COUNT 장을 넘어선 안됩니다." }
+        require(images.size >= MIN_IMAGE_COUNT) { "이미지의 개수는 $MIN_IMAGE_COUNT 장 이상이여야 합니다." }
+        require(images.size <= MAX_IMAGE_COUNT) { "이미지의 개수는 $MAX_IMAGE_COUNT 장을 넘어선 안됩니다." }
     }
 
     companion object {
         private const val MAX_IMAGE_COUNT = 3
+        private const val MIN_IMAGE_COUNT = 0
     }
 }
