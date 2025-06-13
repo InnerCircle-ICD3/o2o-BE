b7b0cb844af9ab085ae4efe9d48e01ccb6d3f51a
youngDaLee <ldy0956@gmail.com>
2025-06-14 01:13:35 +0900

    feat: enum으로 수정

--- eatngo-core/src/main/kotlin/com/eatngo/search/dto/SearchSuggestionResultDto.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/search/dto/SearchSuggestionResultDto.kt
@@ -1,5 +1,7 @@
 package com.eatngo.search.dto
 
+import com.eatngo.search.constant.SuggestionType
+
 data class SearchSuggestionResultDto(
     val suggestionList: List<SearchSuggestionDto>, // 추천 검색어 목록
 ) {
@@ -23,13 +25,13 @@ data class SearchSuggestionResultDto(
 
 data class SearchSuggestionDto(
     val value: String, // 추천 검색어
-    val field: String, // 추천 검색어 출처 필드(예: 매장 or 음식) TODO: enum으로 변경 필요
+    val field: SuggestionType, // 추천 검색어 출처 필드(예: 매장 or 음식)
     val storeId: Long? = null, // 추천 검색어가 매장명인 경우 매장 ID
 ) {
     companion object {
         fun from(
             value: String,
-            field: String,
+            field: SuggestionType,
             storeId: Long? = null,
         ): SearchSuggestionDto =
             SearchSuggestionDto(
