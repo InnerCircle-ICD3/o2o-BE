9fa9784a7d208a08c1034d05dfe0bf9cd91219ff
youngDaLee <ldy0956@gmail.com>
2025-06-14 11:52:29 +0900

    fix: ttl

--- eatngo-infra/src/main/kotlin/com/eatngo/redis/repository/search/SearchSuggestionRedisRepositoryImpl.kt
+++ eatngo-infra/src/main/kotlin/com/eatngo/redis/repository/search/SearchSuggestionRedisRepositoryImpl.kt
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
+import java.time.Duration
 
 /**
  * 추천 검색어 캐싱
@@ -20,11 +21,19 @@ class SearchSuggestionRedisRepositoryImpl(
     @Autowired
     private val objectMapper: ObjectMapper,
 ) : SearchSuggestionRedisRepository {
+    companion object {
+        private const val DEFAULT_TTL_SECONDS = 12 * 60 * 60L // 12시간
+    }
+
     override fun getKey(keyword: String): String = "searchSuggestion:$keyword"
 
     override fun getSuggestsByKey(key: String): List<SearchSuggestionDto> {
         val ops = redisTemplate.opsForHash<String, String>()
         val allValues = ops.entries(key)
+
+        // TTL 연장 TODO: 비동기 구현
+        redisTemplate.expire(key, Duration.ofSeconds(DEFAULT_TTL_SECONDS))
+
         return allValues.values.map {
             objectMapper.readValueFromJson(it)
         }
@@ -39,5 +48,7 @@ class SearchSuggestionRedisRepositoryImpl(
             val json = objectMapper.writeValueToJson(suggestion)
             ops.put(key, suggestion.value, json)
         }
+        // TTL 설정
+        redisTemplate.expire(key, Duration.ofSeconds(DEFAULT_TTL_SECONDS))
     }
 }
