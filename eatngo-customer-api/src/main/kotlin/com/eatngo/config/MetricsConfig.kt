2f9b549348f2f79eb47fd3f37a9e8ab3c6d79b41
hj4645 <hjpark4645@gmail.com>
2025-06-15 01:56:46 +0900

    fix: PR 리뷰 반영

--- eatngo-customer-api/src/main/kotlin/com/eatngo/config/MetricsConfig.kt
+++ eatngo-customer-api/src/main/kotlin/com/eatngo/config/MetricsConfig.kt
@@ -2,8 +2,6 @@ package com.eatngo.config
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.config.MeterFilter
-import io.micrometer.prometheusmetrics.PrometheusConfig
-import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
--- eatngo-store-owner-api/src/main/kotlin/com/eatngo/config/MetricsConfig.kt
+++ eatngo-store-owner-api/src/main/kotlin/com/eatngo/config/MetricsConfig.kt
@@ -2,14 +2,9 @@ package com.eatngo.config
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.config.MeterFilter
-import io.micrometer.core.instrument.simple.SimpleMeterRegistry
-import io.micrometer.prometheusmetrics.PrometheusConfig
-import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.Duration
 
 @Configuration
 class MetricsConfig {
