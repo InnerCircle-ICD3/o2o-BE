3fae1a8ac47c6efdb68c953c3bc94ba08d89064f
youngDaLee <ldy0956@gmail.com>
2025-06-15 22:34:47 +0900

    fix:코드래빗 피드백 수정

--- eatngo-core/src/main/kotlin/com/eatngo/search/domain/SearchStore.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/search/domain/SearchStore.kt
@@ -18,14 +18,14 @@ class SearchStore(
     val storeCategory: List<StoreEnum.StoreCategory>, // 매장 카테고리
     val foodCategory: List<String>, // 대표 판매 음식 종류
     var foodTypes: List<String>?, // 대표 판매 음식 종류(검색용)
-    val roadNameAddress: String? = null, // 매장 주소(도로명주소)
+    val roadNameAddress: String?, // 매장 주소(도로명주소)
     val coordinate: Coordinate, // 매장 위치(위도, 경도)
     val status: SearchStoreStatus, // 매장 오픈 여부
     val businessHours: Map<DayOfWeek, TimeRange>, // 매장 영업 시간
-    var totalReviewCount: Int? = 0, // 총 리뷰 수
-    var averageRating: BigDecimal? = BigDecimal.ZERO, // 평균 평점
-    val paginationToken: String? = null, // 검색 paginationToken
+    var totalReviewCount: Int?, // 총 리뷰 수
+    var averageRating: BigDecimal?, // 평균 평점
+    val paginationToken: String?, // 검색 paginationToken
     val updatedAt: LocalDateTime, // 마지막 업데이트 시간
     val createdAt: LocalDateTime, // 생성 시간
-    val deletedAt: LocalDateTime? = null, // 삭제 시간 (null: 삭제 안됨, not null: 삭제됨) -> RDB에서만 사용
+    val deletedAt: LocalDateTime?, // 삭제 시간 (null: 삭제 안됨, not null: 삭제됨) -> RDB에서만 사용
 )
