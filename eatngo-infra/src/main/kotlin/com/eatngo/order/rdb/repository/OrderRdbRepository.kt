343839c25fc0d875d9714a9031bc9dd7f20a34b5
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-06-18 00:07:09 +0900

    fix: timestamp casting 이슈 해결

--- eatngo-infra/src/main/kotlin/com/eatngo/order/rdb/repository/OrderRdbRepository.kt
+++ eatngo-infra/src/main/kotlin/com/eatngo/order/rdb/repository/OrderRdbRepository.kt
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import java.time.LocalDateTime
 import java.util.*
 
@@ -24,16 +23,16 @@ interface OrderRdbRepository : JpaRepository<OrderJpaEntity, Long> {
         AND (:customerId IS NULL OR o.customerId = :customerId)
         AND (:storeId IS NULL OR o.storeId = :storeId)
         AND (:lastId IS NULL OR o.id < :lastId)
-        AND (:updatedAt IS NULL OR o.updatedAt <= :updatedAt)
-      ORDER BY o.id DESC
+        AND (CAST(:updatedAt AS timestamp) IS NULL OR o.updatedAt <= CAST(:updatedAt AS timestamp))
+      ORDER BY o.id DESC 
     """,
     )
     fun cursoredFindAllByStatus(
-        @Param("status") status: Status?,
-        @Param("customerId") customerId: Long?,
-        @Param("storeId") storeId: Long?,
-        @Param("lastId") lastId: Long?,
-        @Param("updatedAt") updatedAt: LocalDateTime?,
+        status: Status?,
+        customerId: Long?,
+        storeId: Long?,
+        lastId: Long?,
+        updatedAt: LocalDateTime?,
         pageable: Pageable,
     ): Slice<OrderJpaEntity>
 }
