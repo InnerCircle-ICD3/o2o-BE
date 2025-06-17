dcacb85efbcd3e91d01a8ec36d4da4b5dca5ea30
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-06-17 21:30:20 +0900

    fix: OrderReadyEvent 에서 OrderCreatedEvent 로 변경

--- eatngo-core/src/main/kotlin/com/eatngo/order/event/OrderStatusChangedEvent.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/order/event/OrderStatusChangedEvent.kt
@@ -3,32 +3,36 @@ package com.eatngo.order.event
 import com.eatngo.order.domain.Order
 import com.eatngo.order.domain.Status
 
-interface OrderEvent{
-    companion object{
-        fun from(order: Order, userId: Long): OrderEvent?{
-            return when(order.status){
-                Status.CREATED -> null
-                Status.READY -> OrderReadyEvent(
-                    order = order,
-                    userId = userId
-                )
+interface OrderEvent {
+    companion object {
+        fun from(
+            order: Order,
+            userId: Long,
+        ): OrderEvent? =
+            when (order.status) {
+                Status.CREATED ->
+                    OrderCreatedEvent(
+                        order = order,
+                        userId = userId,
+                    )
+                Status.READY -> null
                 Status.CONFIRMED -> null
-                Status.CANCELED -> OrderCanceledEvent(
-                    order = order,
-                    userId = userId
-                )
+                Status.CANCELED ->
+                    OrderCanceledEvent(
+                        order = order,
+                        userId = userId,
+                    )
                 Status.DONE -> null
             }
-        }
     }
 }
 
-data class OrderReadyEvent(
+data class OrderCreatedEvent(
     val userId: Long,
-    val order: Order
-): OrderEvent
+    val order: Order,
+) : OrderEvent
 
 data class OrderCanceledEvent(
     val userId: Long,
-    val order: Order
-): OrderEvent
+    val order: Order,
+) : OrderEvent
