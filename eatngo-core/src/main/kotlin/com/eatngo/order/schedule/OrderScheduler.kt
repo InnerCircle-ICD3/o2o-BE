a641f1bebcad026f67b8ec10fba9b58c03edd30c
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-06-17 21:36:16 +0900

    fix: shedLock 추가 및 의존성 추가

--- eatngo-core/src/main/kotlin/com/eatngo/order/schedule/OrderScheduler.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/order/schedule/OrderScheduler.kt
@@ -1,6 +1,7 @@
 package com.eatngo.order.schedule
 
 import com.eatngo.order.usecase.OldOrderCancelUseCase
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -12,6 +13,11 @@ class OrderScheduler(
     private val logger = LoggerFactory.getLogger(OrderScheduler::class.java)
 
     @Scheduled(cron = "0 */5 * * * *") // 매 5분 마다 실행
+    @SchedulerLock(
+        name = "myUniqueRedisJob",
+        lockAtMostFor = "5m", // 락을 최대 10분까지 유지
+        lockAtLeastFor = "4m", // 최소 30초는 락 유지
+    )
     fun execute() {
         logger.info("Starting oldOrder Cancel")
         kotlin
