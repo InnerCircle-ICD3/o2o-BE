b8310de648c6bc71b64bd3d1a5c57a904379742c
youngDaLee <ldy0956@gmail.com>
2025-06-19 23:26:55 +0900

    fix: 박스 계산 로직 수정

--- eatngo-core/src/main/kotlin/com/eatngo/common/util/MathUtil.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/common/util/MathUtil.kt
@@ -3,8 +3,16 @@ package com.eatngo.common.util
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-fun BigDecimal.normalizeFloor(unit: BigDecimal): BigDecimal =
-    this.divide(unit, 0, RoundingMode.FLOOR).multiply(unit).setScale(unit.scale(), RoundingMode.HALF_UP)
+fun BigDecimal.customNormalizeFloor(unit: BigDecimal): BigDecimal =
+    if (this.remainder(unit).compareTo(BigDecimal.ZERO) == 0) {
+        this
+    } else {
+        this.divide(unit, 0, RoundingMode.FLOOR).multiply(unit)
+    }
 
-fun BigDecimal.normalizeCeil(unit: BigDecimal): BigDecimal =
-    this.divide(unit, 0, RoundingMode.CEILING).multiply(unit).setScale(unit.scale(), RoundingMode.HALF_UP)
+fun BigDecimal.customNormalizeCeil(unit: BigDecimal): BigDecimal =
+    if (this.remainder(unit).compareTo(BigDecimal.ZERO) == 0) {
+        this
+    } else {
+        this.divide(unit, 0, RoundingMode.CEILING).multiply(unit)
+    }
