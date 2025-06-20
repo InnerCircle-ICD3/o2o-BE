bbaaf61d8eae4e9baf749dee128b42f3d8a5b30d
Kyungtae Park <pktpkt8917@gmail.com>
2025-06-20 23:33:40 +0900

    fix/customer-address fix query

--- eatngo-infra/src/main/kotlin/com/eatngo/customer/rdb/repository/CustomerAddressRdbRepository.kt
+++ eatngo-infra/src/main/kotlin/com/eatngo/customer/rdb/repository/CustomerAddressRdbRepository.kt
@@ -3,6 +3,7 @@ package com.eatngo.customer.rdb.repository
 import com.eatngo.common.type.CoordinateVO
 import com.eatngo.customer.domain.CustomerAddressType
 import com.eatngo.customer.rdb.entity.CustomerAddressJpaEntity
+import com.eatngo.customer.rdb.entity.CustomerJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.util.*
@@ -27,7 +28,7 @@ interface CustomerAddressRdbRepository : JpaRepository<CustomerAddressJpaEntity,
         """
     )
     fun findByAddress(
-        customerId: Long?,
+        customer: CustomerJpaEntity?,
         radiusInKilometers: Double,
         customerAddressType: CustomerAddressType,
         coordinate: CoordinateVO
