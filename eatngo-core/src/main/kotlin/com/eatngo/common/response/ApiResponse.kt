fa3e72006457f62e3e286ec3143dbccd9325ce1d
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-06-23 23:02:05 +0900

    fix: 에러 발생 시 data 를 전달할 수 있도록 수정

--- eatngo-core/src/main/kotlin/com/eatngo/common/response/ApiResponse.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/common/response/ApiResponse.kt
@@ -1,27 +1,27 @@
 package com.eatngo.common.response
 
-
 sealed class ApiResponse<T> {
     abstract val success: Boolean
 
     data class Success<T>(
         override val success: Boolean = true,
-        val data: T
+        val data: T,
     ) : ApiResponse<T>()
-    
+
     data class Error<T>(
         override val success: Boolean = false,
         val errorCode: String,
-        val errorMessage: String
+        val errorMessage: String,
+        val data: Map<String, Any>? = null,
     ) : ApiResponse<T>()
-    
-    companion object {
-        fun <T> success(data: T): ApiResponse<T> {
-            return Success(data = data)
-        }
 
-        fun <T> error(errorCode: String, errorMessage: String): ApiResponse<T> {
-            return Error(errorCode = errorCode, errorMessage = errorMessage)
-        }
+    companion object {
+        fun <T> success(data: T): ApiResponse<T> = Success(data = data)
+
+        fun <T> error(
+            errorCode: String,
+            errorMessage: String,
+            data: Map<String, Any>? = null,
+        ): ApiResponse<T> = Error(errorCode = errorCode, errorMessage = errorMessage, data = data)
     }
-} 
+}
