63f0cd13e63632b32756a87feaa1a6d00a0d7cb3
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-06-23 22:29:21 +0900

    feat: 생성되어있는 주문 중에 예약 확정하지 않은 주문이 있다면 에러 발생

--- eatngo-core/src/main/kotlin/com/eatngo/common/exception/order/OrderException.kt
+++ eatngo-core/src/main/kotlin/com/eatngo/common/exception/order/OrderException.kt
@@ -10,19 +10,28 @@ open class OrderException(
     override val data: Map<String, Any>? = null,
     override val logLevel: Level = Level.WARN,
 ) : BusinessException(errorCode, message, data, logLevel) {
-
     // 예외 생성
-    class OrderNotFound(orderId: Long) : OrderException(
-        errorCode = BusinessErrorCode.ORDER_NOT_FOUND,
-        message = "${BusinessErrorCode.ORDER_NOT_FOUND.message} (ID: $orderId)",
-        data = mapOf("orderId" to orderId)
-    )
+    class OrderNotFound(
+        orderId: Long,
+    ) : OrderException(
+            errorCode = BusinessErrorCode.ORDER_NOT_FOUND,
+            message = "${BusinessErrorCode.ORDER_NOT_FOUND.message} (ID: $orderId)",
+            data = mapOf("orderId" to orderId),
+        )
 
-    class OrderAlreadyCompleted(orderId: Long) : OrderException(
-        errorCode = BusinessErrorCode.ORDER_ALREADY_COMPLETED,
-        message = "${BusinessErrorCode.ORDER_ALREADY_COMPLETED.message} (ID: $orderId)",
-        data = mapOf("orderId" to orderId)
-    )
+    class OrderAlreadyCompleted(
+        orderId: Long,
+    ) : OrderException(
+            errorCode = BusinessErrorCode.ORDER_ALREADY_COMPLETED,
+            message = "${BusinessErrorCode.ORDER_ALREADY_COMPLETED.message} (ID: $orderId)",
+            data = mapOf("orderId" to orderId),
+        )
 
-    // 추후 작성
+    class OrderCreatedAlreadyExists(
+        orderIds: List<Long>,
+    ) : OrderException(
+            errorCode = BusinessErrorCode.ORDER_CREATED_ALREADY_EXISTS,
+            message = "${BusinessErrorCode.ORDER_CREATED_ALREADY_EXISTS.message} (ID: ${orderIds.joinToString(",")})",
+            data = mapOf("orderIds" to orderIds),
+        )
 }
